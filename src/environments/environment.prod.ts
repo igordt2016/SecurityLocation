035f61d64e3944a262698c464670ab64c5b2602c
Igor Lima <igorleonardo728@gmail.com>
2019-12-02 20:24:39 -0300

    Ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC6q7xOeKqu--np3kZBUBa9Y1LvgXvd4wE",
+    authDomain: "angularproject-440e3.firebaseapp.com",
+    databaseURL: "https://angularproject-440e3.firebaseio.com",
+    projectId: "angularproject-440e3",
+    storageBucket: "",
+    messagingSenderId: "941573512510",
+    appId: "1:941573512510:web:5a3197cb07ab6ccaac606f"
+  }
 };
