5e1f42f56f77a5a2244de6ee08fba9c306d0b21a
Igor Lima <igorleonardo728@gmail.com>
2019-12-04 00:55:02 -0300

    Heroku Integração.

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SecurityLocation'));
+app.use(express.static(__dirname + '/SecurityLocation'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/SecurityLocation/index.html'));
+res.sendFile(path.join(__dirname+'/SecurityLocation/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
